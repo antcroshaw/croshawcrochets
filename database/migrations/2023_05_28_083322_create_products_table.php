2d5c8943475695161bf369ac48686e0775681662
antcroshaw <antcroshaw@outlook.com>
2023-06-04 10:52:12 +0100

    migration and resource route added for products

--- database/migrations/2023_05_28_083322_create_products_table.php
+++ database/migrations/2023_05_28_083322_create_products_table.php
@@ -13,6 +13,11 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->longText('description');
+            $table->decimal('price');
+            $table->string('image');
+            $table->boolean('available')->default('1');
             $table->timestamps();
         });
     }
