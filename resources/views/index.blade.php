06137861cfd132b09dbc9a00b91ce1b812ab146f
Anthony Croshaw <anthonycroshaw@anthonys-air-m2.home>
2023-07-18 07:07:21 +0100

    moving to bootstrap 5

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,226 +1,39 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="utf-8">
+    <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="">
-    <meta name="author" content="">
-    <title>Bootstrap E-Commerce Template- DIGI Shop mini</title>
-    <!-- Bootstrap core CSS -->
-    <link href="{{  asset('css/bootstrap.css') }}" rel="stylesheet">
-    <!-- Fontawesome core CSS -->
-    <link href="{{ asset('css/font-awesome.min.css') }}" rel="stylesheet" />
-    <!--GOOGLE FONT -->
-    <link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
-    <!--Slide Show Css -->
-    <link href=" {{ asset('ItemSlider/css/main-style.css') }}" rel="stylesheet" />
-    <!-- custom CSS here -->
-    <link href=" {{ asset('css/style.css')}} " rel="stylesheet" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+    <title>Frontend Bootcamp</title>
 </head>
+
 <body>
-    <nav class="navbar navbar-default" role="navigation">
-        <div class="container-fluid">
-            <!-- Brand and toggle get grouped for better mobile display -->
-            <div class="navbar-header">
-                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-                    <span class="sr-only">Toggle navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
-                <a class="navbar-brand" href="#"><strong>DIGI</strong> Shop</a>
-            </div>
+    <nav class="navbar navbar-expand-lg bg-dark navbar-dark">
+        <div class="container">
+            <a href="#" class="navbar-brand">Frontend Bootcamp</a>
 
-            <!-- Collect the nav links, forms, and other content for toggling -->
-            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-
-
-                <ul class="nav navbar-nav navbar-right">
-                    <li><a href="#">Track Order</a></li>
-                  
-                       @auth
-                       <li><a href="{{ route('home') }}">admin</a></li>
-                       @else
-                       <li><a href="{{ route('login') }}">Login</a></li>
-                       @endauth
-                  
-                    <li><a href="#">Signup</a></li>
-
-                    <li class="dropdown">
-                        <a href="#" class="dropdown-toggle" data-toggle="dropdown">24x7 Support <b class="caret"></b></a>
-                        <ul class="dropdown-menu">
-                            <li><a href="#"><strong>Call: </strong>+09-456-567-890</a></li>
-                            <li><a href="#"><strong>Mail: </strong>info@yourdomain.com</a></li>
-                            <li class="divider"></li>
-                            <li><a href="#"><strong>Address: </strong>
-                                <div>
-                                    234, New york Street,<br />
-                                    Just Location, USA
-                                </div>
-                            </a></li>
-                        </ul>
+            <button class="navbar-toggler" type="buttono" data-bs-toggle="collapse" data-bs-target="#navmenu">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navmenu">
+                <ul class="navbar-nav ms-auto">
+                    <li class="nav-item"><a href="#learn" class="nav-link">What You'll Learn</a>
+                    </li>
+                    <li class="nav-item"><a href="#questions" class="nav-link">Questions</a>
+                    </li>
+                    <li class="nav-item"><a href="#instructors" class="nav-link">Instructors</a>
                     </li>
                 </ul>
-                <form class="navbar-form navbar-right" role="search">
-                    <div class="form-group">
-                        <input type="text" placeholder="Enter Keyword Here ..." class="form-control">
-                    </div>
-                    &nbsp; 
-                    <button type="submit" class="btn btn-primary">Search</button>
-                </form>
             </div>
-            <!-- /.navbar-collapse -->
         </div>
-        <!-- /.container-fluid -->
     </nav>
-    <div class="container">
-        <div class="row">
-            <div class="col-md-12">
-                <div class="well well-lg offer-box text-center">
 
-
-                   Today's Offer : &nbsp; <span class="glyphicon glyphicon-cog"></span>&nbsp;40 % off  on purchase of $ 2,000 and above till 24 dec !                
-              
-               
-                </div>
-                <div class="main box-border">
-                    <div id="main_image" class="main_image">
-                     <img src="{{ asset('img/main_image.jpg') }}" width="100%">
-                    </div>
-                    
-                </div>
-                <br />
-            </div>
-            <!-- /.col -->
-            
-          
-        </div>
-        <!-- /.row -->
-        <div class="row">
-            <div class="col-md-3">
-                <div>
-                    <a href="#" class="list-group-item active">Electronics
-                    </a>
-                    <ul class="list-group">
-
-                        <li class="list-group-item">Mobile
-      <span class="label label-primary pull-right">234</span>
-                        </li>
-                        <li class="list-group-item">Computers
-                      <span class="label label-success pull-right">34</span>
-                        </li>
-                        <li class="list-group-item">Tablets
-                         <span class="label label-danger pull-right">4</span>
-                        </li>
-                        <li class="list-group-item">Appliances
-                             <span class="label label-info pull-right">434</span>
-                        </li>
-                        <li class="list-group-item">Games & Entertainment
-                             <span class="label label-success pull-right">34</span>
-                        </li>
-                    </ul>
-                </div>
-                <!-- /.div -->
-                <div>
-                    <a href="#" class="list-group-item active list-group-item-success">Clothing & Wears
-                    </a>
-                    <ul class="list-group">
-
-                        <li class="list-group-item">Men's Clothing
-                             <span class="label label-danger pull-right">300</span>
-                        </li>
-                        <li class="list-group-item">Women's Clothing
-                             <span class="label label-success pull-right">340</span>
-                        </li>
-                        <li class="list-group-item">Kid's Wear
-                             <span class="label label-info pull-right">735</span>
-                        </li>
-
-                    </ul>
-                </div>
-                <!-- /.div -->
-              
-                <!-- /.div -->
-                <div>
-                    <ul class="list-group">
-                        <li class="list-group-item list-group-item-success"><a href="#">New Offer's Coming </a></li>
-                        <li class="list-group-item list-group-item-info"><a href="#">New Products Added</a></li>
-                        <li class="list-group-item list-group-item-warning"><a href="#">Ending Soon Offers</a></li>
-                        <li class="list-group-item list-group-item-danger"><a href="#">Just Ended Offers</a></li>
-                    </ul>
-                </div>
-                <!-- /.div -->
-                <div class="well well-lg offer-box offer-colors">
-
-
-                    <span class="glyphicon glyphicon-star-empty"></span>25 % off  , GRAB IT                 
-              
-                   <br />
-                    <br />
-                    <div class="progress progress-striped">
-                        <div class="progress-bar progress-bar-danger" role="progressbar" aria-valuenow="70" aria-valuemin="0" aria-valuemax="100"
-                            style="width: 70%">
-                            <span class="sr-only">70% Complete (success)</span>
-                            2hr 35 mins left
-                        </div>
-                    </div>
-                    <a href="#">click here to know more </a>
-                </div>
-                <!-- /.div -->
-            </div>
-            <!-- /.col -->
-            <div class="col-md-9">
-                <div>
-                    <ol class="breadcrumb">
-                        <li><a href="#">Home</a></li>
-                        <li class="active">Electronics</li>
-                    </ol>
-                </div>
-                <!-- /.div -->
-               
-                
-              
-                <div class="row">
-                    @foreach ($products as $product)
-                    <div class="col-md-4 text-center col-sm-6 col-xs-6">
-                        <div class="thumbnail product-box">
-                            <img src="{{ url('storage/images/' . $product->image) }}" alt="" height="100%" />
-                            <div class="caption">
-                                <h3><a href="#">{{ $product->name }} </a></h3>
-                                <p>Price : <strong>$ 3,45,900</strong>  </p>
-                               
-                                    <a href="#" class="btn btn-success" role="button">Add To Cart</a>
-                                    <a href="#" class="btn btn-primary" role="button">See Details</a>
-                                </p>
-                            </div>
-                        </div>
-                    </div>
-                    @endforeach
-                  
-                </div>
-            </div>
-        </div>
-        <hr>
-    </div>
-    <!-- /.col -->
-    <div class="col-md-12 end-box ">
-        &copy; 2014 | &nbsp; All Rights Reserved | &nbsp; www.yourdomain.com | &nbsp; 24x7 support | &nbsp; Email us: info@yourdomain.com
-    </div>
-    <!-- /.col -->
-    <!--Footer end -->
-    <!--Core JavaScript file  -->
-    <script src="{{ asset('js/jquery-1.10.2.js') }}"></script>
-    <!--bootstrap JavaScript file  -->
-    <script src="{{ asset('js/bootstrap.js') }}"></script>
-    <!--Slider JavaScript file  -->
-    <script src="{{ asset('ItemSlider/js/modernizr.custom.63321.js') }}"></script>
-    <script src="{{ asset('ItemSlider/js/jquery.catslider.js') }}"></script>
-    <script>
-        $(function () {
-
-            $('#mi-slider').catslider();
-
-        });
-		</script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
+        crossorigin="anonymous"></script>
 </body>
+
 </html>
