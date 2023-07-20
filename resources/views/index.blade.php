5a770b0829135b4446bca764c921c82f2db58b90
Anthony Croshaw <anthonycroshaw@anthonys-air-m2.home>
2023-07-20 05:35:30 +0100

    changed to local bootstrap and css files

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,19 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
+<x-layout>
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <link rel="stylesheet" href="style.css">
-    <title>Frontend Bootcamp</title>
-</head>
-
-<body>
     <nav class="navbar navbar-expand-lg bg-dark navbar-dark">
         <div class="container">
-            <a href="#" class="navbar-brand">Frontend Bootcamp</a>
+            <a href="#" class="navbar-brand">Croshaw Crochets</a>
 
             <button class="navbar-toggler" type="buttono" data-bs-toggle="collapse" data-bs-target="#navmenu">
                 <span class="navbar-toggler-icon"></span>
@@ -31,9 +20,7 @@
         </div>
     </nav>
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
-        crossorigin="anonymous"></script>
-</body>
+    <script src=" {{ asset('js/bootstrap.bundle.min.js') }}"></script>
 
-</html>
+
+</x-layout>
