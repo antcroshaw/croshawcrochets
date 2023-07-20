912b0b10cc8d12472115554ac9669ae55619cdce
antcroshaw <antcroshaw@outlook.com>
2023-07-20 05:43:32 +0100

    uptodate with other site

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,26 +1,28 @@
 <x-layout>
 
-    <nav class="navbar navbar-expand-lg bg-dark navbar-dark">
-        <div class="container">
-            <a href="#" class="navbar-brand">Croshaw Crochets</a>
+ 
 
-            <button class="navbar-toggler" type="buttono" data-bs-toggle="collapse" data-bs-target="#navmenu">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navmenu">
-                <ul class="navbar-nav ms-auto">
-                    <li class="nav-item"><a href="#learn" class="nav-link">What You'll Learn</a>
-                    </li>
-                    <li class="nav-item"><a href="#questions" class="nav-link">Questions</a>
-                    </li>
-                    <li class="nav-item"><a href="#instructors" class="nav-link">Instructors</a>
-                    </li>
-                </ul>
+    <section class="bg-dark text-light p-5 p-lg-0 pt-lg-5 text-center text-sm-start">
+        <div class="container">
+            <div class="d-sm-flex align-items-center justify-content-between">
+                <div>
+                    <h1>Become a <span class="text-warning">Web Developer</span></h1>
+                    <p class="lead my-4">We focus on teaching our students the fundamentals of the latest and greatest 
+                    technologies to prepare them for their first dev role</p>
+                    <button class="btn btn-primary btn-lg">Start the Enrollment</button>
+                </div>
+                <img class="img-fluid w-50 d-none d-sm-block" src="{{ asset('img/showcase.svg') }}" alt="">
             </div>
         </div>
-    </nav>
+    </section>
 
-    <script src=" {{ asset('js/bootstrap.bundle.min.js') }}"></script>
-
+    <!-- Newsletter -->
+    <section class="bg-primary text-light p-5">
+        <div class="container">
+            <div class="d-md-flex justify-content-between align-items-center">
+                <h3 class="mb-3 mb-md-0">Sign up for our Newsletter</h3>
+            </div>
+        </div>
+    </section>
 
 </x-layout>
